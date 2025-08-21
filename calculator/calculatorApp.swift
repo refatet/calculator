1ea05dcc7fbee9b0f867927053b561094037d751
refatet <g_cola3402@icloud.com>
2025-08-21 18:40:15 +0900

    Initial Commit

---
+++ calculator/calculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  calculatorApp.swift
+//  calculator
+//
+//  Created by 윤요한 on 8/21/25.
+//
+
+import SwiftUI
+
+@main
+struct calculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ calculatorTests/calculatorTests.swift
@@ -0,0 +1,17 @@
+//
+//  calculatorTests.swift
+//  calculatorTests
+//
+//  Created by 윤요한 on 8/21/25.
+//
+
+import Testing
+@testable import calculator
+
+struct calculatorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
